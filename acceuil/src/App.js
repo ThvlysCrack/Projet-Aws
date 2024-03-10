b39237a3e83333db7a266f9bde239be8c96f13e4
aymen bouzerkoune <aymen-bzrk@outlook.com>
2024-03-10 14:26:42 +0100

    global NavBar + Build Folder

--- acceuil/src/App.js
+++ acceuil/src/App.js
@@ -1,11 +1,12 @@
 import Accueil from './Composants/Acceuil/acceuil';
 import LoginSignup from './Composants/LoginSignup/LoginSignup';
 import BrowsingRoutes from './BrowsingRoutes';
+import Test from './Composants/Dailys/Classic/Test';
 
 function App() {
   return (
     <div>
-      {/*<Accueil/> <LoginSignup/> */}
+      {/* <BrowsingRoutes/> */}
       <BrowsingRoutes/>
       
     </div>
