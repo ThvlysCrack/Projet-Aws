19dd25223c156fdaab7ffb9b7b4ff758241be8a1
aymen bouzerkoune <aymen-bzrk@outlook.com>
2024-04-05 14:28:47 +0200

    login implemented

--- acceuil/src/Composants/LoginSignup/LoginSignup.jsx
+++ acceuil/src/Composants/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import './LoginSignup.css'
 import user_icon from '../assets/images/person.png'
 import email_icon from '../assets/images/email.png'
@@ -8,33 +9,75 @@ import backgroundImage from '../assets/images/Giratina.png';
 import Backgroundtest from '../Backgroundtest';
 
 const LoginSignup = () => {
-    const[action,setAction] = useState("CONNEXION");
-  return (
-    <Backgroundtest image={backgroundImage}>
-    <div className="container">
-        <img src={logo} alt="" className="logo" />
-        <div className="inputs">
-            <div className="input">
-                <img src={user_icon} alt=''/>
-                <input type="text" placeholder='Pseudo'/>
+    const [email, setEmail] = useState('');
+    const [pseudo, setPseudo] = useState('');
+    const [password, setPassword] = useState('');
+    const [action, setAction] = useState('CONNEXION');
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/login', { email, password });
+            if (response.data.status === 'ok') {
+                // Login was successful
+                console.log('Token:', response.data.data);
+            } else {
+                // Login failed
+                console.log('Error:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Error making request:', error);
+        }
+    };
+    const handleRegister = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/register', { pseudo, email, password });
+            if (response.data.status === 'ok') {
+                // Registration was successful
+                console.log('User registered successfully');
+            } else {
+                // Registration failed
+                console.log('Error:', response.data.error);
+            }
+        } catch (error) {
+            console.error('Error making request:', error);
+        }
+    };
+    const handleAction = () => {
+        if (action === "CONNEXION") {
+            handleLogin();
+        } else {
+            handleRegister();
+        }
+    };
+
+    return (
+        <Backgroundtest image={backgroundImage}>
+            <div className="container">
+                <img src={logo} alt="" className="logo" />
+                <div className="inputs">
+                    {action === "CONNEXION" ? <div></div> : <div className="input">
+                        <img src={user_icon} alt='' />
+                        <input type="text" placeholder='Pseudo' value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+                    </div>}
+                    <div className="input">
+                        <img src={email_icon} alt='' />
+                        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+
+                    <div className="input">
+                        <img src={password_icon} alt='' />
+                        <input type="password" placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button onClick={handleAction}>Submit</button>
+                </div>
+                {action === "INSCRIPTION" ? <div></div> : <div className="forgot-password"><span>Mot de passe oublié?</span></div>}
+
+                <div className="submit-container">
+                    <div className={action === "CONNEXION" ? "submit gray" : "submit"} onClick={() => { setAction("INSCRIPTION") }}>INSCRIPTION</div>
+                    <div className={action === "INSCRIPTION" ? "submit gray" : "submit"} onClick={() => { setAction("CONNEXION") }}>CONNEXION</div>
+                </div>
             </div>
-            {action==="CONNEXION"?<div></div>:<div className="input">
-                <img src={email_icon} alt=''/>
-                <input type="email" placeholder='Email'/>
-                </div>}
-            
-            <div className="input">
-                <img src={password_icon} alt=''/>
-                <input type="password" placeholder='Mot de passe'/>
-            </div>
-        </div>
-        {action=== "INSCRIPTION"?<div></div>:   <div className="forgot-password"><span>Mot de passe oublié?</span></div>}
-        <div className="submit-container">
-            <div className={action==="CONNEXION"?"submit gray":"submit"} onClick={()=>{setAction("INSCRIPTION")}}>INSCRIPTION</div>
-            <div className={action==="INSCRIPTION"?"submit gray":"submit"} onClick={()=>{setAction("CONNEXION")}}>CONNEXION</div>
-        </div>
-    </div>
-    </Backgroundtest>
-  )
+        </Backgroundtest>
+    )
 }
 export default LoginSignup;
