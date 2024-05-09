a75a1c4829e69e0d6f7209ca72b29f40b281e933
Latoui Asma uvsq22308962 <asma.latoui002@gmail.com>
2024-05-09 13:18:33 +0200

    j'arrivais pas à faire un push sur mon autre branche

--- acceuil/Backend/Routes.js
+++ acceuil/Backend/Routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
-const { default: mongoose } = require("mongoose");
+// const { default: mongoose } = require("mongoose");
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
@@ -10,7 +10,9 @@ var nodemailer = require('nodemailer');
 var path = require('path');
 const { User, DailyPokemon, PlayerAdvancement } = require('./Schema');
 const { appendFile } = require("fs");
-
+const { body, validationResult } = require('express-validator')
+const mongoose = require("mongoose");
+require("dotenv").config();
 router.get('/', (req, res) => {
   res.send(`
   <!DOCTYPE html>
@@ -45,27 +47,39 @@ router.get('/', (req, res) => {
 `);
 });
 
-router.post("/register", async (req, res) => {
-  const { pseudo, email, password } = req.body;
-  
-  try {
-    if (!pseudo || !email || !password) {
-      throw new Error("Missing required fields");
-    }
-    const salt = await bcrypt.genSalt();
-    const encryptedPassword = await bcrypt.hash(password, salt);
-    console.log('comparison result:', encryptedPassword);
-    const oldUser = await User.findOne({ email });
-    if (oldUser) {
-      console.log("User already exists");
-      return res.status(400).json({ error: "User already exists" });
+router.post("/register", 
+  // Express Validator middleware
+  [
+    body('pseudo').trim().notEmpty().withMessage('Le pseudo est requis'),
+    body('email').trim().isEmail().withMessage('Veuillez entrer un email valide'),
+    body('password').trim().isLength({ min: 8 }).withMessage('Le mot de passe doit comporter au moins 8 caractères'),
+    body('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/).withMessage('Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const newUser = await User.create({
-      pseudo,
-      email,
-      password: encryptedPassword,
-    });
+    // If validation passes, proceed with registration
+    const { pseudo, email, password } = req.body;
+
+    try {
+      const salt = await bcrypt.genSalt();
+      const encryptedPassword = await bcrypt.hash(password, salt);
+      console.log('comparison result:', encryptedPassword);
+      const oldUser = await User.findOne({ email });
+      if (oldUser) {
+        console.log("User already exists");
+        return res.status(400).json({ error: "User already exists" });
+      }
+
+      const newUser = await User.create({
+        pseudo,
+        email,
+        password: encryptedPassword,
+      });
 
     const pAdvancement = await PlayerAdvancement.create({
       userId: newUser._id,
@@ -85,68 +99,101 @@ router.post("/register", async (req, res) => {
 });
 
 
-router.post("/login", async (req, res) => {
-  try {
-  const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ error: "Email and password are required" });
-  }
-  const user = await User.findOne({ email: email});
-  //console.log('comparison result:', user);
-  if (!user) { return res.json({ error: "User Not found" });  }
+router.post("/login", 
+  // Express Validator middleware
+  [
+    body('email').trim().isEmail().withMessage('Veuillez entrer un email valide'),
+    body('password').trim().isLength({ min: 8 }).withMessage('Le mot de passe doit comporter au moins 8 caractères'),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  const isPasswordCorrect = await bcrypt.compare(password, user.password);
-  if (!isPasswordCorrect) {
-    return res.status(401).json({ status: "error", error: "InvAlid Password" });
-   }
-   const token = jwt.sign({ email: user.email }, process.env.JWT_Secret, {
-    expiresIn: "15m", });
-    console.log('connexion successful');
-    return res.status(200).json({ status: "ok", data: { token: token, userId: user._id }  });
-  } catch (error) {console.error('Error in login route:', error);
-  return res.status(500).json({ error: error.message });
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({ error: "Email and password are required" });
+      }
+      const user = await User.findOne({ email: email});
+      //console.log('comparison result:', user);
+      if (!user) { 
+        return res.json({ error: "User Not found" });  
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        return res.status(401).json({ status: "error", error: "Invalid Password" });
+      }
+      
+      const token = jwt.sign({ email: user.email }, process.env.JWT_Secret, {
+        expiresIn: "15m", 
+      });
+      
+      console.log('connexion successful');
+      return res.status(200).json({ status: "ok", data: { token: token, userId: user._id }  });
+    } catch (error) {
+      console.error('Error in login route:', error);
+      return res.status(500).json({ error: error.message });
+    }
   }
+);
+
+
+
+router.post("/forgot-password",
+  // Express Validator middleware
+  [
+    body('email').trim().isEmail().withMessage('Veuillez entrer un email valide'),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email } = req.body;
+    try {
+      const oldUser = await User.findOne({ email });
+      if (!oldUser) {
+        return res.json({ status: "User Not Exists!!" });
+      }
+      const secret = process.env.JWT_Secret + oldUser.password;
+      const token = jwt.sign({ email: oldUser.email,}, secret, {
+        expiresIn: "5m",
+      });
+      const link = `http://localhost:4000/reset-password/${oldUser._id}/${token}`;
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL_USERNAME, //oldUser.email
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      var mailOptions = {
+        from: "noreply@gmail.com",
+        to: email,
+        subject: "Password Reset",
+        text: link,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      console.log(link);
+    } catch (error) { }
   });
 
-
-router.post("/forgot-password", async (req, res) => {
-  const { email } = req.body;
-  try {
-    const oldUser = await User.findOne({ email });
-    if (!oldUser) {
-      return res.json({ status: "User Not Exists!!" });
-    }
-    const secret = process.env.JWT_Secret + oldUser.password;
-    const token = jwt.sign({ email: oldUser.email,}, secret, {
-      expiresIn: "5m",
-    });
-    const link = `http://localhost:4000/reset-password/${oldUser._id}/${token}`;
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USERNAME, //oldUser.email
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-
-    var mailOptions = {
-      from: "noreply@gmail.com",
-      to: email,
-      subject: "Password Reset",
-      text: link,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-    console.log(link);
-  } catch (error) { }
-});
 
 router.get("/reset-password/:id/:token", async (req, res) => {
   const { id, token } = req.params;
@@ -164,35 +211,47 @@ router.get("/reset-password/:id/:token", async (req, res) => {
     res.send("Not Verified");
   }
 });
-router.post("/reset-password/:id/:token", async (req, res) => {
-  const { id, token } = req.params;
-  const { password } = req.body;
+router.post("/reset-password/:id/:token", 
+  // Express Validator middleware
+  [
+    body('password').trim().isLength({ min: 8 }).withMessage('Le mot de passe doit comporter au moins 8 caractères'),
+    body('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])/).withMessage('Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { id, token } = req.params;
+    const { password } = req.body;
 
-  const oldUser = await User.findOne({ _id: id });
-  if (!oldUser) {
-    return res.json({ status: "User Not Exists!!" });
-  }
-  const secret = process.env.JWT_Secret + oldUser.password;
-  try {
-    const verify = jwt.verify(token, secret);
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    await User.updateOne(
-      {
-        _id: id,
-      },
-      {
-        $set: {
-          password: encryptedPassword,
+    const oldUser = await User.findOne({ _id: id });
+    if (!oldUser) {
+      return res.json({ status: "User Not Exists!!" });
+    }
+    const secret = process.env.JWT_Secret + oldUser.password;
+    try {
+      const verify = jwt.verify(token, secret);
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      await User.updateOne(
+        {
+          _id: id,
         },
-      }
-    );
+        {
+          $set: {
+            password: encryptedPassword,
+          },
+        }
+      );
 
-    res.render("index", { email: verify.email, status: "verified" });
-  } catch (error) {
-    console.log(error);
-    res.json({ status: "Something Went Wrong" });
+      res.render("index", { email: verify.email, status: "verified" });
+    } catch (error) {
+      console.log(error);
+      res.json({ status: "Something Went Wrong" });
+    }
   }
-});
+);
 
 router.get('/daily-pokemons', async (req, res) => {
   try {
@@ -260,7 +319,17 @@ router.post('/game1Advancement/add/:userId', async (req, res) => {
     return res.status(500).json({ error: 'Erreur lors de l\'ajout de l\'élément à la liste game1Advancement' });
   }
 });
+const mongoUrl =
+  "mongodb+srv://asma:SCI123@cluster0.nuo1d42.mongodb.net/POKEDLE?retryWrites=true&w=majority";
 
-
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
 
 module.exports = router;
--- acceuil/src/Composants/LoginSignup/LoginSignup.jsx
+++ acceuil/src/Composants/LoginSignup/LoginSignup.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios'
-import './LoginSignup.css'
-import user_icon from '../assets/images/person.png'
-import email_icon from '../assets/images/email.png'
-import password_icon from '../assets/images/password.png'
-import logo from '../assets/images/PokeZapv2.png'
-//import backgroundImage from '../assets/images/Giratina.png';
+import axios from 'axios';
+import './LoginSignup.css';
+import user_icon from '../assets/images/person.png';
+import email_icon from '../assets/images/email.png';
+import password_icon from '../assets/images/password.png';
+import logo from '../assets/images/PokeZapv2.png';
 import backgroundImage from '../assets/images/background102.jpg';
 import Backgroundtest from '../Backgroundtest';
 
@@ -15,6 +14,11 @@ const LoginSignup = () => {
     const [pseudo, setPseudo] = useState('');
     const [password, setPassword] = useState('');
     const [action, setAction] = useState('CONNEXION');
+    const [errors, setErrors] = useState({
+        pseudo: '',
+        email: '',
+        password: ''
+    });
     const navigate = useNavigate();
     
     const handleLogin = async () => {
@@ -28,12 +32,13 @@ const LoginSignup = () => {
                 navigate('/Defis-quotidiens');
             } else {
                 // Login failed
-                console.log('Error:', response.data.error);
+                setErrors({ ...errors, email: response.data.error });
             }
         } catch (error) {
             console.error('Error making request:', error);
         }
     };
+
     const handleRegister = async () => {
         try {
             const response = await axios.post('https://pokezapserver.vercel.app/register', { pseudo, email, password });
@@ -42,42 +47,90 @@ const LoginSignup = () => {
                 console.log('User registered successfully');
             } else {
                 // Registration failed
-                console.log('Error:', response.data.error);
+                setErrors({ ...errors, email: response.data.error });
             }
         } catch (error) {
             console.error('Error making request:', error);
         }
     };
-    const handleForgotPassword = () => {}
+
+    const handleForgotPassword = () => {};
+
     const handleAction = () => {
-        if (action === "CONNEXION") {
-            handleLogin();
+        let isValid = true;
+        const newErrors = { email: '', pseudo: '', password: '' };
+
+        if (!pseudo) {
+            isValid = false;
+            newErrors.pseudo = 'Le pseudo est requis';
+        }
+
+        if (!email) {
+            isValid = false;
+            newErrors.email = 'L\'email est requis';
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            isValid = false;
+            newErrors.email = 'Veuillez entrer un email valide';
+        }
+
+        if (!password) {
+            isValid = false;
+            newErrors.password = 'Le mot de passe est requis';
+        } else if (password.length < 8) { 
+            isValid = false;
+            newErrors.password = 'Le mot de passe doit comporter au moins 8 caractères';
+        } else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_])/.test(password)) {
+            isValid = false;
+            newErrors.password = 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial';
+        }
+        
+
+        if (isValid) {
+            if (action === "CONNEXION") {
+                handleLogin();
+            } else {
+                handleRegister();
+            }
         } else {
-            handleRegister();
+            setErrors(newErrors);
         }
     };
 
+    
     return (
         <Backgroundtest image={backgroundImage}>
             <div className="container">
                 <div className="inputs">
-                    {action === "CONNEXION" ? <div></div> : <div className="input">
-                        <img src={user_icon} alt='' />
-                        <input type="text" placeholder='Pseudo' value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
-                    </div>}
-                    <div className="input">
-                        <img src={email_icon} alt='' />
-                        <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    {action === "CONNEXION" ? null : (
+                        <div className="input-container">
+                            <div className="input">
+                                <img src={user_icon} alt='' />
+                                <input type="text" placeholder='Pseudo' value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+                            </div>
+                            {errors.pseudo && <p className="error">{errors.pseudo}</p>}
+                        </div>
+                    )}
+                    <div className="input-container">
+                        <div className="input">
+                            <img src={email_icon} alt='' />
+                            <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        {errors.email && <p className="error">{errors.email}</p>}
                     </div>
-
-                    <div className="input">
-                        <img src={password_icon} alt='' />
-                        <input type="password" placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div className="input-container">
+                        <div className="input">
+                            <img src={password_icon} alt='' />
+                            <input type="password" placeholder='Mot de passe' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        {errors.password && <p className="error">{errors.password}</p>}
                     </div>
                     <button className="SubmitButton" onClick={handleAction}>Submit</button>
                 </div>
-                {action === "INSCRIPTION" ? <div></div> : <div className="forgot-password"><a href="/forgot-password"><span>Mot de passe oublié?</span></a></div>}
-
+                {action === "INSCRIPTION" ? null : (
+                    <div className="forgot-password">
+                        <a href="/forgot-password"><span>Mot de passe oublié?</span></a>
+                    </div>
+                )}
                 <div className="submit-container">
                     <div className={action === "CONNEXION" ? "submit gray" : "submit"} onClick={() => { setAction("INSCRIPTION") }}>INSCRIPTION</div>
                     <div className={action === "INSCRIPTION" ? "submit gray" : "submit"} onClick={() => { setAction("CONNEXION") }}>CONNEXION</div>
@@ -86,4 +139,5 @@ const LoginSignup = () => {
         </Backgroundtest>
     )
 }
+
 export default LoginSignup;
