b016a30d8e6d57df0a5b55de410cf04fea2905ff
Latoui Asma uvsq22308962 <asma.latoui002@gmail.com>
2024-05-12 13:08:04 +0200

    classement+css

--- acceuil/src/Composants/Classement/Classement.jsx
+++ acceuil/src/Composants/Classement/Classement.jsx
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Classement.css';
 import bgImage from '../assets/images/stadepokemon2.png';
 import trophy from '../assets/images/win.jpg';
-import badge from '../assets/images/badgegold.png'
+import badge from '../assets/images/badgegold.png';
+import master from '../assets/images/Master-Ball.png';
+import hyper from '../assets/images/HyperBall.png';
+import superb from '../assets/images/SuperBall.png';
+import poke from '../assets/images/PokeBall.png';
 
 function Classement() {
   const [searchText, setSearchText] = useState('');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchTopUsers() {
+      try {
+        const response = await axios.get('http://localhost:4000/top-users');
+        const data = response.data;
+        console.log(data)
+        setUsers(data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des utilisateurs:', error);
+      }
+    }
+
+    fetchTopUsers();
+  }, []);
 
   const handleSearch = (event) => {
     setSearchText(event.target.value.toLowerCase());
   };
 
-  const players = [
-    { rank: 1, name: 'Ilyes', score: 239 },
-    { rank: 2, name: 'Aymen', score: 209 },
-    { rank: 3, name: 'Elias', score: 154 },
-    { rank: 4, name: 'Asma', score: 100 },
-    { rank: 5, name: 'Yann', score: 82 },
-    { rank: 1, name: 'Ilyes', score: 239 },
-    { rank: 2, name: 'Aymen', score: 209 },
-    { rank: 3, name: 'Elias', score: 154 },
-    { rank: 4, name: 'Asma', score: 100 },
-    { rank: 5, name: 'Yann', score: 82 },
-  ];
-
-  const filteredPlayers = players.filter(player =>
-    player.name.toLowerCase().includes(searchText)
+  const filteredUsers = users.filter(user =>
+    user.pseudo.toLowerCase().includes(searchText)
   );
 
   return (
@@ -37,7 +45,7 @@ function Classement() {
       </div>
       <div className="leaderboard-container">
         <div className="description">
-          <p>Dernière mise à jour : <br/>Aujourd'hui à 11PM</p>
+          <p>Dernière mise à jour : <br/>Aujourd'hui à 12AM</p>
           <input
             id="search"
             className="search"
@@ -55,11 +63,16 @@ function Classement() {
             </tr>
           </thead>
           <tbody>
-            {filteredPlayers.map((player, index) => (
+            {filteredUsers.map((user, index) => (
               <tr key={index} className={index === 0 ? 'winner' : index === 1 ? 'runner-up' : index === 2 ? 'second-runner-up' : ''}>
-                <td className="rank">{player.rank}</td>
-                <td>{player.name}</td>
-                <td>{player.score}</td>
+                <td className="rank">
+                  {index === 0 && <img src={master} alt="Master Ball" />}
+                  {index === 1 && <img src={hyper} alt="Hyper Ball" />}
+                  {index === 2 && <img src={superb} alt="Super Ball" />}
+                  {(index !== 0 && index !== 1 && index !== 2) && <img src={poke} alt="Poke Ball" />}
+                </td>
+                <td>{user.pseudo}</td>
+                <td>{user.score}</td>
               </tr>
             ))}
           </tbody>
@@ -67,12 +80,13 @@ function Classement() {
       </div>
       <div className='messagecontainer'>
         <img src={trophy} alt='Trophy' className='trophyImage' />
-        <p className='message'>Découvrez les 10 meilleurs joueurs du monde ! Parcourez la liste ci-dessous pour connaître leurs surnoms, classements et scores.</p>
+        <p className='message'>Découvrez les 10 meilleurs joueurs du monde ! Parcourez la liste ci-dessous pour connaître
+          leurs surnoms, classements et scores.</p>
       </div>
 
       <div className='messagecontainer2'>
         <img src={badge} alt='gold' className='badgeImage' />
-        <p className='message2nd'> Les classements sont basés sur la notation en jeu de la veille. <br/> Le classement sera mis à jour tous les jours à 23h00 UTC.</p>
+        <p className='message2nd'> Les classements sont basés sur la notation en jeu de la veille. <br/> Le classement sera mis à jour tous les jours à minuit</p>
       </div>
    </body> 
   );
